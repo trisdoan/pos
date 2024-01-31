3cd41595bf8232be479964f2de355d7f929740b5
trisdoan <doanminhtri8183@gmail.com>
2024-01-31 17:55:13 +0700

    [ADD] pos_receipt_hide_user

---
+++ pos_receipt_hide_user/models/pos_config.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class PosConfig(models.Model):
+    _inherit = "pos.config"
+
+    hide_user = fields.Boolean(string="Hide User in POS receipt?", default=True)
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    hide_user = fields.Boolean(related="pos_config_id.hide_user", readonly=False)
