d652b4d15b671f484ec0cb536cbc9b197f2cc0db
trisdoan <doanminhtri8183@gmail.com>
2024-04-23 18:17:29 +0700

    [ADD] pos_sell_by_packaging

---
+++ pos_sell_by_packaging/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "POS Sell By Packaging",
+    "summary": "", # TODO: fill me
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "category": "Point of Sale",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "version": "16.0.1.0.0",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "views/product_template.xml"
+    ]
+}
